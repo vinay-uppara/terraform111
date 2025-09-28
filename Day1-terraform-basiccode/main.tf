3f67cf1aceabf82068767965b34d188038dd6ec7
uppara <upparavinay2003n@gmail.com>
2025-09-28 15:22:49 +0530

    day1

---
+++ Day1-terraform-basiccode/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+     tags = {
+       name= "prod"
+     }
+  
+}
---
+++ Day1-terraform-basiccode/variable.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    description = ""
+    type = string
+    default = ""
+  
+}
+
+variable "instance_type" {
+    description = "value"
+    type = string
+    default = ""
+  
+}
+
+variable "key_name" {
+    description = "value"
+    type = string
+    default = ""
+  
+}
